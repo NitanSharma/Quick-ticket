0364c02f972671a6b30aa8d9f0ce98f1dccdd155
NitanSharma <sharmanitan938@gmail.com>
2025-08-28 09:05:47 +0530

    Sentery Helper function added

--- src/actions/ticket.actions.ts
+++ src/actions/ticket.actions.ts
@@ -3,6 +3,7 @@
 import * as Sentry from '@sentry/nextjs'
 import {prisma} from '@/db/prisma';
 import { revalidatePath } from 'next/cache';
+import { logEvent } from '@/utils/sentry';
 
 
 export async function createTicket(prevState :{success : boolean; message : string} ,formData : FormData) : Promise<{success: boolean; message :string}>{
@@ -16,7 +17,8 @@ export async function createTicket(prevState :{success : boolean; message : stri
     console.log(subject , description , priority);
 
     if(!subject || !description || !priority ){
-        Sentry.captureMessage('Validation Error : Missing ticket fields' , 'warning')
+        // Sentry.captureMessage('Validation Error : Missing ticket fields' , 'warning');
+        logEvent('Validation Error : Missing ticket data fields', 'ticket' , {subject , description , priority} , 'warning' )
         return {success: false , message : 'All fields are required'}
     }
 
@@ -25,21 +27,36 @@ export async function createTicket(prevState :{success : boolean; message : stri
         data: {subject , description , priority}
     })
 
-    Sentry.addBreadcrumb({
-        category : 'ticket',
-        message : `Ticket created : ${ticket.id}`,
-        level : 'info'
-    })
+    logEvent(`Ticket created successfully:${ticket.id}`, 'ticket' , {ticketId : ticket.id} , 'info')
 
-    Sentry.captureException(`Ticket was created successfully : ${ticket.id}`);
+    // Sentry.addBreadcrumb({
+    //     category : 'ticket',
+    //     message : `Ticket created : ${ticket.id}`,
+    //     level : 'info'
+    // })
+    // Sentry.captureException(`Ticket was created successfully : ${ticket.id}`);
 
     revalidatePath('/tickets');
 
     return {success: true, message : 'Ticket created successfully'};
+
     } catch (error) {
-        Sentry.captureException(error as Error, {
-        extra: { formData: Object.fromEntries(formData.entries()) },
-});
+
+
+        logEvent(
+            'An error occured while creating the ticket',
+            'ticket',
+            {
+                formData: Object.fromEntries(formData.entries()),
+            },
+            'error',
+            error
+        );
+
+    //     Sentry.captureException(error as Error, {
+    //     extra: { formData: Object.fromEntries(formData.entries()) },
+    //    });
+    
         return {success: false , message : 'An error occured while creating the ticket'}
     }
 
